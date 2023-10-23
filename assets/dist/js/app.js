5414d2d088e23713c484b765585eddc917a6dd96
Simpoze James <simpocodes@gmail.com>
2023-10-23 20:32:10 +0200

    v 2.0.05
    new updates

--- assets/dist/js/app.js
+++ assets/dist/js/app.js
@@ -1,29 +1,19 @@
 //?=================== showingthe nav ===============
-
 $('.menu').on({
     click: menuclick => {
-        if ($(document).innerWidth() > 424) {
-            $('nav').addClass('right-0')
-            $('nav').removeClass('top-0')
-        } else if ($(document).innerWidth() < 424) {
-            $('nav').addClass('top-0')
-            // alert('alert')
-        }
-
+        $('nav').addClass('d-flex')
     }
 })
+//------------------------ showingthe nav
+
+
 
 //?=================== closing the nav ===============
 $('nav ul .close').click(e => {
-    if ($(document).innerWidth() > 425.9) {
-        $('nav').removeClass('right-0')
-    } else if ($(document).innerWidth() < 425) {
-        $('nav').removeClass('top-0')
-
-
-    }
-
+    $('nav').removeClass('d-flex')
 })
+//---------------- End of closing the nav
+
 
 
 //?=================== gsap animations ===============
@@ -31,9 +21,6 @@ gsap.from('.logo a', { color: 'blue' })
 const timeline = gsap.timeline({ defaults: { duration: 1 } })
 timeline
     .from('ul li a', { opacity: 0, stagger: .3 })
-    .from('.article ', { opacity: 0, stagger: .2 })
-
-
 
 gsap.from('.menu', { opacity: 0, delay: .3 })
 gsap.from('header', { opacity: 0, duration: 2, delay: .3 })
@@ -41,19 +28,15 @@ gsap.from('header .header-empty', { scale: 0, duration: 4, delay: .3 })
 gsap.from('.header-text h1', { x: -50, duration: 1, delay: .6 })
 gsap.from('.header-text p', { x: 50, duration: 1, delay: .6 })
 gsap.from('.header-img', { opacity: 0, scale: .7, duration: 1, delay: 1.5, ease: 'elastic' })
-gsap.to('.sticky', { y: 0, ease: 'elastic' })
-// gsap.from('.section-body-img', { opacity: 0, duration: .3 })
-// gsap.from('.section-body-text', { opacity: 0, duration: .3, })
+//---------------- End of gsap animations
 
 
 //*=========== on window scroll
-
 $(window).on({
     scroll: sc => {
         if ($(document).scrollTop() > 80) {
             $('#navigation').fadeIn()
             $('#navigation').addClass('sticky')
-            // $('.top-0').addClass('sticky')
 
         } else if ($(document).scrollTop() == 0) {
             $('#navigation').removeClass('sticky')
@@ -80,7 +63,9 @@ productZoom.forEach(e => {
     })
 })
 
+//---------------- End of on window scroll
 
+//*=========== empty div
 $('.empty .close').on({
     click: cc => {
         cc.preventDefault();
@@ -88,4 +73,5 @@ $('.empty .close').on({
         empty.classList.add('d-none')
     }
 })
+//---------------- End of empty div
 
